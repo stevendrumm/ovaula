4663b75a7dedd487fc526779e52f04b36f3de93d
stevendrumm <andret199377@hotmail.com>
2016-02-28 21:25:38 -0500

    index 9:25

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE>
+<html>
+<head>
+	<meta name="keywords" content="<?php 
+	//aprendisaje, escuelas, pruebas, examen, santander de quilichao, santander, institucion// 
+	">
+	<meta http-equiv="Content-Script-Type" content="text/javascript">
+	<meta http-equiv="Content-Style-Type" content="text/css">
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-5">
+	<meta http-equiv="Content-Language" content="es"/>
+	<title><?php echo "" ?></title>
+</head>
+<body>
+	
+</body>
+</html>
