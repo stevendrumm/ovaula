69cd986f405f5cd3f0111e509227ca30f3dbb4f5
stevendrumm <andret199377@hotmail.com>
2016-02-28 21:31:40 -0500

    index 9:31

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
 	<meta name="keywords" content="<?php 
 	//aprendisaje, escuelas, pruebas, examen, santander de quilichao, santander, institucion// 
-	">
+	?>">
 	<meta http-equiv="Content-Script-Type" content="text/javascript">
 	<meta http-equiv="Content-Style-Type" content="text/css">
 	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-5">
 	<meta http-equiv="Content-Language" content="es"/>
+	<meta name="Robots" content="all"/>
 	<title><?php echo "" ?></title>
 </head>
 <body>
